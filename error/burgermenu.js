94dcf879d6186457714370136d6da37a7b9e0e8e
chinaraliyeva <chinara.gulieva@mail.ru>
2022-06-22 00:27:39 +0400

    Folders refreshed

--- error/burgermenu.js
+++
@@ -1,6 +0,0 @@
-const menu = document.querySelector('.menu')
-const burgerMenu = document.querySelector('.burger-menu')
-
-burgerMenu.addEventListener("click", function(){
-    menu.classList.toggle('show');
-})
